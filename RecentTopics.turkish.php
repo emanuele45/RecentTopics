e256d1ac7a4516fa6eada02b64984a33e9bbad45
gevv <320voltmail@gmail.com>
2016-11-13 13:50:26 +0200

    Create RecentTopics.turkish.php

---
+++ RecentTopics.turkish.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Recent Topics
+ *
+ * @name      Recent Topics
+ * @copyright Recent Topics contributors
+ * @license   BSD http://opensource.org/licenses/BSD-3-Clause
+ *
+ * @version 0.0.4
+ *
+ */
+
+
+$txt['recent_posts'] = 'Son konular';
